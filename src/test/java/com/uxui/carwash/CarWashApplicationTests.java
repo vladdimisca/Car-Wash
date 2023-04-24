b3810106fdc67446731f0e0d5e2d94707c410ede
Vlad <vladdimisca@gmail.com>
2023-04-24 11:54:27 +0300

    CW-1: Use h2 profile for all ITs

--- src/test/java/com/uxui/carwash/CarWashApplicationTests.java
+++ src/test/java/com/uxui/carwash/CarWashApplicationTests.java
@@ -2,8 +2,10 @@ package com.uxui.carwash;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("h2")
 class CarWashApplicationTests {
 
     @Test
