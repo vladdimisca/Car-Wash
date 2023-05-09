599d3ff9c2c06654ba44c5695012a8bc9b515b57
Cristian <cristian_anton99@yahoo.com>
2023-05-09 18:41:58 +0300

    CW-6 : add service creation and view

--- src/main/java/com/uxui/carwash/controller/JobController.java
+++ src/main/java/com/uxui/carwash/controller/JobController.java
@@ -1,12 +1,16 @@
 package com.uxui.carwash.controller;
 
+import com.uxui.carwash.error.exception.AbstractApiException;
+import com.uxui.carwash.model.Job;
 import com.uxui.carwash.service.JobService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/jobs")
@@ -14,6 +18,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class JobController {
 
     private final JobService jobService;
+
+    @PostMapping
+    public String create(@Valid @ModelAttribute(name = "service") Job job, BindingResult bindingResult, RedirectAttributes attr) {
+        if (bindingResult.hasErrors()) {
+            return "job-form";
+        }
+
+        try {
+            jobService.create(job);
+        } catch (AbstractApiException e) {
+            attr.addFlashAttribute("service", job);
+            attr.addFlashAttribute("api_error", e.getMessage());
+            return "redirect:/jobs/form";
+        }
+        return "redirect:/jobs";
+    }
+
+    @PutMapping("/{id}")
+    public String update(@PathVariable("id") Long id, @Valid @ModelAttribute(name = "service") Job job, BindingResult bindingResult, RedirectAttributes attr) {
+        if (bindingResult.hasErrors()) {
+            return "update-job-form";
+        }
+
+        try {
+            jobService.update(id, job);
+        } catch (AbstractApiException e) {
+            attr.addFlashAttribute("service", job);
+            attr.addFlashAttribute("api_error", e.getMessage());
+            return "redirect:/jobs/form/" + id;
+        }
+        return "redirect:/jobs";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteById(@PathVariable("id") Long id) {
+        jobService.deleteById(id);
+        return "redirect:/jobs";
+    }
 
     @GetMapping
     public String getAll(Model model) {
@@ -25,5 +67,21 @@ public class JobController {
     public String getById(@PathVariable("id") Long id, Model model) {
         model.addAttribute("job", jobService.getById(id));
         return "job-info";
+    }
+
+    @GetMapping("/form")
+    public String jobForm(Model model) {
+        if (!model.containsAttribute("service")) {
+            model.addAttribute("service", new Job());
+        }
+        return "job-form";
+    }
+
+    @GetMapping("/form/{id}")
+    public String updateJobForm(@PathVariable("id") Long id, Model model) {
+        if (!model.containsAttribute("service")) {
+            model.addAttribute("service", jobService.getById(id));
+        }
+        return "update-job-form";
     }
 }
